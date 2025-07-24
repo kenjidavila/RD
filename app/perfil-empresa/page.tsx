436401289589719ff6615921fa24ec9795816701
kenjidavila <ken2694@gmail.com>
2025-07-23 19:14:17 -0600

    wrap perfil empresa page with configuracion provider

--- app/perfil-empresa/page.tsx
+++ app/perfil-empresa/page.tsx
@@ -1,5 +1,12 @@
 "use client"
 import PerfilEmpresa from "@/components/configuracion/perfil-empresa"
+import { ConfiguracionTabsProvider } from "@/components/configuracion/configuracion-tabs-context"
+
+function noop() {}
 export default function PerfilEmpresaPage() {
-  return <PerfilEmpresa />
+  return (
+    <ConfiguracionTabsProvider reportError={noop} reportSuccess={noop}>
+      <PerfilEmpresa />
+    </ConfiguracionTabsProvider>
+  )
 }
